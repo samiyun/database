e2e2d9cf44a670bd88a43dcf3fef54fa2593777b
samiyun <samiyun.uddin@gmail.com>
2026-06-14 20:01:03 -0400

    simple REPL implement
    Only recognizes .exit as a command

--- db.c
+++ db.c
@@ -4,6 +4,59 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+
+/*
+  we have to define InputBuffer as a small wrapper 
+  around the state we need to store with getLine().
+*/
+typedef struct {
+  char* buffer;
+  size_t buffer_length;
+  ssize_t input_length;
+} InputBuffer;
+
+InputBuffer* new_input_buffer() { 
+  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
+  input_buffer->buffer = NULL;
+  input_buffer->buffer_length = 0;
+  input_buffer->input_length = 0;
+
+  return input_buffer;
+}
+
+void print_prompt() { 
+  printf("db > "); 
+}
+
+/*
+  we tell getline to store the read line in input_buffer->buffer 
+  and the size of the allocated buffer in input_buffer->buffer_length. 
+  we store the return value in input_buffer->input_length.
+
+  'buffer' starts as null, so getline allocates enough memory to 
+  hold the line of input and makes buffer point to it.
+
+*/
+void read_input(InputBuffer* input_buffer) {
+  ssize_t bytes_read =
+      getline(&(input_buffer->buffer), &(input_buffer->buffer_length), stdin);
+
+  if (bytes_read <= 0) {
+    printf("Error reading input\n");
+    exit(EXIT_FAILURE);
+  }
+
+  // ignore trailing newline
+  input_buffer->input_length = bytes_read - 1;
+  input_buffer->buffer[bytes_read - 1] = 0;
+}
+
+void close_input_buffer(InputBuffer* input_buffer) {
+    free(input_buffer->buffer);
+    free(input_buffer);
+}
 
 /*
   sqlite starts a read-execute-print loop when you start it from the command line
@@ -24,23 +77,3 @@ int main(int argc, char* argv[]) {
     }
   }
 }
-/*
-  now we have to define InputBuffer as a small wrapper 
-  around the state we need to store with getLine().
-*/
-typedef struct {
-  char* buffer;
-  size_t buffer_length;
-  ssize_t input_length;
-} InputBuffer;
-
-InputBuffer* new_input_buffer() {
-  InputBuffer* input_buffer = (InputBuffer*)malloc(sizeof(InputBuffer));
-  input_buffer->buffer = NULL;
-  input_buffer->buffer_length = 0;
-  input_buffer->input_length = 0;
-
-  return input_buffer;
-}
-
-//hello world
